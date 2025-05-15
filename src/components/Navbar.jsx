e4c2cea8c59756c434eb996882c5a052e5c8aa59
Thivakar <thivakar.parthiban@gmail.com>
2025-05-15 21:14:41 +0530

    Remove logo image from Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
               window.scrollTo(0, 0);
             }}
           >
-            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
             <p className="text-white text-[20px] font-bold cursor-pointer flex">
               Thivakar&nbsp;
               <span className="sm:block hidden">PARTHIBAN</span>
